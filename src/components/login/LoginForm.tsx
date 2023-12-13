a29fac3a2c56949874fdc1cfa228e830bd634059
kiritoEM <johankirito64@gmail.com>
2023-12-13 09:55:46 +0100

    add auth-google template

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,19 +1,29 @@
-const LoginForm = () : JSX.Element => {
-    return (
-        <div className="section-form mt-5">
-              <form action="post">
-                  <div className="email">
-                      <input type="text" placeholder="Email" id="email-input"/>
-                  </div>
-                  <div className="password">
-                      <input type="text" placeholder="Mot de Passe" id="email-input"/>
-                  </div>
-                  <div className="button">
-                      <button className="btn">Se connecter</button>
-                  </div>
-              </form>
-        </div>
-    );
+const LoginForm = (): JSX.Element => {
+  return (
+    <div className="section-form mt-5">
+      <div className="form">
+        <form action="post">
+          <div className="email">
+            <input type="text" placeholder="Email" id="email-input" />
+          </div>
+          <div className="password">
+            <input type="text" placeholder="Mot de Passe" id="email-input" />
+          </div>
+          <div className="button">
+            <button className="btn">Se connecter</button>
+          </div>
+        </form>
+      </div>
+      <div className="or">
+          <p>OU</p>
+      </div>
+      <div className="auth-google">
+          <button className="btn">
+              <img src="/images/icon/google.png" alt="" className="mx-2" /> Continuer avec Google
+          </button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
