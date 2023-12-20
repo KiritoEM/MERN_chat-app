0ba31560b116001ce02b4989c47cc24e63d26b2c
kiritoEM <johankirito64@gmail.com>
2023-12-20 13:58:04 +0100

    add header sticky

--- src/components/Home/ChatList.tsx
+++ src/components/Home/ChatList.tsx
@@ -3,10 +3,19 @@ import DiscussionCard from "./ChatList/DiscussionCard";
 const ChatList = (): JSX.Element => {
   return (
     <section id="chat-list">
-      <div className="section-logo">
-        <img src="/logo-white.png" alt="" />
+      <div className="section-header">
+        <div className="header-logo">
+          {" "}
+          <img src="/logo-white.png" alt="" />
+        </div>
+        <div className="profile">
+          <img src="/images/hiro.jpeg" alt="" />
+        </div>
       </div>
       <div className="section-list">
+        <DiscussionCard />
+        <DiscussionCard />
+        <DiscussionCard />
         <DiscussionCard />
         <DiscussionCard />
         <DiscussionCard />
