66cc6599a56ddc445ded9afa2534e2f67445eb6e
kiritoEM <johankirito64@gmail.com>
2023-12-25 14:43:31 +0100

    fix messages affichage

--- src/components/Home/ChatContent/ChatContentMessages.tsx
+++ src/components/Home/ChatContent/ChatContentMessages.tsx
@@ -17,11 +17,11 @@ const ChatContentMessages: React.FC<IProps> = ({ messages }): JSX.Element => {
     <div className="section-messages mt-5">
       {messages.map((item, index) => (
         <div
-          className={item.author === userID ? "chat-1" : "chat-2"}
+          className={item.author._id === userID ? "chat-2" : "chat-1"}
           key={index}
         >
           <div className="content">
-            <div className={`profile-picture ${item.author===userID ? "" : "d-none"}`}>
+            <div className={`profile-picture ${item.author._id ===userID ? "d-none" : ""}`}>
               <img src="/images/hiro.jpeg" alt="" />
             </div>
             <p>{item.content}</p>
